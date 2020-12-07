af7a09ce893d8b763d61d00a080ec2d9f2c293dd
lihuan <www.babywssl@foxmail.com>
2020-12-07 15:28:50 +0800

    12/07

--- demo04.py
+++ demo04.py
@@ -2,7 +2,10 @@
 class 声明类的名字
 然后类的名字的首字母必须大写
 面向对象编程
+默认属性方法__init__ 使用__init__来接受参数
 类里面所有的方法，都必须要传一个参数，叫self
+
+继承 重写/多态
 """
 
 class Girlfriend():
@@ -45,9 +48,38 @@ class Girlfriend():
         print("你性别为"+self.sex+"身高"+self.high+"体重"+self.weigh+"发型"+self.hair+"年龄"+self.age+"的女朋友开始了自己的工作之:")
         print("开挖掘机！")
 
-# 类的实例化
-zhangsan = Girlfriend("男","188cm","70kg","锡纸烫","24")
+# # 类的实例化
+# zhangsan = Girlfriend("男","188cm","70kg","锡纸烫","24")
+# zhangsan.caiyi(1)
+# zhangsan.chuyi()
+# zhangsan.work()
+# print(zhangsan.sex)
+
+# class Car():
+#     def __init__(self,pingpai,yanse,neishi,jilun):
+#         self.pingpai = pingpai
+#         self.yanse = yanse
+#         self.neishi= neishi
+#         self.jilun = jilun
+
+#     def bianxing(self):
+#         print("车子变身金刚喜羊羊！")
+
+#     def fly(self):
+#         print("车子开始起飞！")
+
+# zhangsan = Car("奥拓","五颜六色","豪华","独轮")
+# zhangsan.bianxing()
+# print(zhangsan.pingpai)
+
+class Nvpengyou(Girlfriend):
+    def work(self):
+        print("修电脑")
+    #pass  #占位 没有作用 防止代码没写完 报错
+
+zhangsan = Nvpengyou("女","165cm","50kg","大波浪","20")
 zhangsan.caiyi(1)
-zhangsan.chuyi()
 zhangsan.work()
-print(zhangsan.sex)
+
+# object 祖宗类
+# Girlfriend 父类     Nvpengyou 子类 可以修改覆盖掉父类的方法叫做重写/多态
